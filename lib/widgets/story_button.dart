789f3991f7158c78652955801494958b3d9b11e6
Mahender Kumar <mksb1947@gmail.com>
2024-09-28 14:34:11 +0530

    comment removed

--- lib/widgets/story_button.dart
+++ lib/widgets/story_button.dart
@@ -11,16 +11,6 @@ class StoryButton extends StatelessWidget {
   StoryButton({super.key, required this.user, required this.tag});
 
   void _onStoryTap(BuildContext context) {
-    // Navigate to the detailed page with a hero animation
-    // Navigator.push(
-    //   context,
-    //   MaterialPageRoute(
-    //     builder: (context) => StoryDetailPage(
-    //       imageUrl: user.profilePicture,
-    //       tag: tag,
-    //     ),
-    //   ),
-    // );
     Navigator.push(
       context,
       MaterialPageRoute(
@@ -28,7 +18,7 @@ class StoryButton extends StatelessWidget {
           stories: [
             ...user.stories,
           ],
-          initialIndex: 0, // Start from the first story
+          initialIndex: 0,
         ),
       ),
     );
@@ -86,204 +76,6 @@ class StoryButton extends StatelessWidget {
   }
 }
 
-// class StoryDetailPage extends StatelessWidget {
-//   final String? imageUrl;
-//   final String tag;
-
-//   const StoryDetailPage({super.key, this.imageUrl, required this.tag});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       body: Stack(
-//         children: [
-//           Center(
-//             child: Hero(
-//               tag: tag, // Same tag as used in the previous screen
-//               child: Image.network(
-//                 imageUrl ?? 'https://randomuser.me/api/portraits/men/7.jpg',
-//                 fit: BoxFit.cover,
-//               ),
-//             ),
-//           ),
-//           Positioned(
-//             top: 40,
-//             left: 16,
-//             child: IconButton(
-//               icon: const Icon(Icons.close, color: Colors.white),
-//               onPressed: () {
-//                 Navigator.pop(context); // Close the story detail
-//               },
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class StoryDetailPage extends StatelessWidget {
-//   final List<String> imageUrls; // List of story image URLs
-//   final int initialIndex; // The index of the initial story to display
-
-//   const StoryDetailPage({
-//     super.key,
-//     required this.imageUrls,
-//     required this.initialIndex,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       body: PageView.builder(
-//         itemCount: imageUrls.length,
-//         controller: PageController(initialPage: initialIndex),
-//         itemBuilder: (context, index) {
-//           return Stack(
-//             children: [
-//               Center(
-//                 child: Hero(
-//                   tag: imageUrls[
-//                       index], // Use the image URL as a tag for the hero animation
-//                   child: Image.network(
-//                     imageUrls[index],
-//                     fit: BoxFit.cover,
-//                   ),
-//                 ),
-//               ),
-//               Positioned(
-//                 top: 40,
-//                 left: 16,
-//                 child: IconButton(
-//                   icon: const Icon(Icons.close, color: Colors.white),
-//                   onPressed: () {
-//                     Navigator.pop(context); // Close the story detail
-//                   },
-//                 ),
-//               ),
-//               // Add more functionality for tapping to change stories
-//               GestureDetector(
-//                 onTap: () {
-//                   // Add any specific action when tapping on the image (if needed)
-//                 },
-//               ),
-//             ],
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// class StoryDetailPage extends StatefulWidget {
-//   final List<Story> stories; // List of story image URLs
-//   final int initialIndex; // The index of the initial story to display
-
-//   const StoryDetailPage({
-//     super.key,
-//     required this.stories,
-//     required this.initialIndex,
-//   });
-
-//   @override
-//   _StoryDetailPageState createState() => _StoryDetailPageState();
-// }
-
-// class _StoryDetailPageState extends State<StoryDetailPage> {
-//   late PageController _pageController;
-//   late int _currentIndex;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _currentIndex = widget.initialIndex;
-//     _pageController = PageController(initialPage: _currentIndex);
-//     _pageController.addListener(() {
-//       setState(() {
-//         _currentIndex = _pageController.page!.round(); // Update current index
-//       });
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     _pageController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.black,
-//       // appBar: AppBar(
-//       //   backgroundColor: Colors.black,
-//       //   automaticallyImplyLeading: false,
-//       //   title: Row(
-//       //     mainAxisAlignment: MainAxisAlignment.center,
-//       //     children: List.generate(widget.imageUrls.length, (index) {
-//       //       return Expanded(
-//       //         child: Container(
-//       //           margin: const EdgeInsets.symmetric(horizontal: 2.0),
-//       //           height: 2.0,
-//       //           decoration: BoxDecoration(
-//       //             color: _currentIndex == index ? Colors.white : Colors.grey,
-//       //             borderRadius: BorderRadius.circular(8.0),
-//       //           ),
-//       //         ),
-//       //       );
-//       //     }),
-//       //   ),
-//       // ),
-//       body: Stack(
-//         children: [
-//           PageView.builder(
-//             itemCount: widget.stories.length,
-//             controller: _pageController,
-//             itemBuilder: (context, index) {
-//               final story = widget.stories[index];
-//               return story.mediaType == 'image'
-//                   ? Center(
-//                       child: Hero(
-//                         tag: widget.stories[
-//                             index], // Use the image URL as a tag for the hero animation
-//                         child: Image.network(
-//                           story.mediaUrl,
-//                           fit: BoxFit.fill,
-//                         ),
-//                       ),
-//                     )
-//                   : Container();
-//             },
-//           ),
-//           Positioned(
-//             top: 80,
-//             left: 0,
-//             right: 0,
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: List.generate(widget.stories.length, (index) {
-//                 return Expanded(
-//                   child: Container(
-//                     margin: const EdgeInsets.symmetric(horizontal: 2.0),
-//                     height: 2.0,
-//                     decoration: BoxDecoration(
-//                       color:
-//                           _currentIndex == index ? Colors.white : Colors.grey,
-//                       borderRadius: BorderRadius.circular(8.0),
-//                     ),
-//                   ),
-//                 );
-//               }),
-//             ),
-//           ),
-//           TitleListTile(),
-//         ],
-//       ),
-//     );
-//   }
-// }
 class StoryDetailPage extends StatefulWidget {
   final List<Story> stories; // List of stories with attributes
   final int initialIndex; // The index of the initial story to display
@@ -379,8 +171,7 @@ class _StoryDetailPageState extends State<StoryDetailPage> {
                       )
                     : VideoApp(
                         onVideoEnd: _onVideoEnd,
-                        videoUrl: story
-                              .mediaUrl,
+                        videoUrl: story.mediaUrl,
                       ); // Pass the callback
               },
             ),
